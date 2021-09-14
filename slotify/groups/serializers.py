04abd76164cbe4d6771499960639c1d54636bc6c
tlylt <liu_yongliang@hotmail.com>
2021-09-14 23:01:55 +0800

    Add back membership

--- slotify/groups/serializers.py
+++ slotify/groups/serializers.py
@@ -3,10 +3,12 @@ from rest_framework import serializers
 from authentication.models import User
 from groups.models import Group, Category, Tag, Membership
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')
+        fields = ("id", "username")
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,11 +23,17 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class MembershipSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=True)
-
     class Meta:
         model = Membership
-        fields = ("id", "user", "group")
+        fields = (
+            "id",
+            "user",
+            "group",
+            "date_joined",
+            "is_approved",
+            "is_admin",
+            "tag",
+        )
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -37,9 +45,12 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "description", "banner_url", "category", "members")
 
     def get_members(self, instance):
-        records = Membership.objects.filter(group=instance).values_list('user', flat=True)
-        users = User.objects.filter(pk__in = records)
+        records = Membership.objects.filter(group=instance).values_list(
+            "user", flat=True
+        )
+        users = User.objects.filter(pk__in=records)
         return UserSerializer(users, many=True).data
+
 
 class GroupCreateSerializer(serializers.ModelSerializer):
     class Meta:
--- slotify/groups/urls.py
+++ slotify/groups/urls.py
@@ -4,18 +4,24 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import groups.api_views
 
 urlpatterns = [
-
-    path('', groups.api_views.GroupList.as_view()),
-    path('new', groups.api_views.GroupCreate.as_view()),
-    path('<int:id>/', groups.api_views.GroupRetrieveUpdateDestroy.as_view()),
-
-    path('tags/', groups.api_views.TagList.as_view()),
-    path('tags/new', groups.api_views.TagCreate.as_view()),
-    path('tags/<int:id>/', groups.api_views.TagRetrieveUpdateDestroy.as_view()),
-
-    path('categories/', groups.api_views.CategoryList.as_view()),
-    path('categories/new', groups.api_views.CategoryCreate.as_view()),
-    path('categories/<int:id>/', groups.api_views.CategoryRetrieveUpdateDestroy.as_view()),
+    path("", groups.api_views.GroupList.as_view()),
+    path("new", groups.api_views.GroupCreate.as_view()),
+    path("<int:id>/", groups.api_views.GroupRetrieveUpdateDestroy.as_view()),
+    path("tags/", groups.api_views.TagList.as_view()),
+    path("tags/new", groups.api_views.TagCreate.as_view()),
+    path("tags/<int:id>/", groups.api_views.TagRetrieveUpdateDestroy.as_view()),
+    path("categories/", groups.api_views.CategoryList.as_view()),
+    path("categories/new", groups.api_views.CategoryCreate.as_view()),
+    path(
+        "categories/<int:id>/", groups.api_views.CategoryRetrieveUpdateDestroy.as_view()
+    ),
+    path("members/", groups.api_views.MembersList.as_view()),
+    path("memberships/", groups.api_views.MembershipList.as_view()),
+    path("memberships/new", groups.api_views.MembershipCreate.as_view()),
+    path(
+        "memberships/<int:id>/",
+        groups.api_views.MembershipRetrieveUpdateDestroy.as_view(),
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
