00c6420ce5725851ea0681c466b8a23df017b32b
Cynthia <leewengyancynthia@gmail.com>
2021-09-23 14:57:12 +0800

    Add signups to admin

--- slotify/events/admin.py
+++ slotify/events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Event, Slot
+from .models import Event, Slot, SignUp
 
 admin.site.register(Event)
 admin.site.register(Slot)
+admin.site.register(SignUp)
